fe96712af69741472cb6a1570e72b8b3e9c67b51
loretta-dhahabu <loreydjefwa@gmail.com>
2022-06-08 20:18:06 +0300

    commit tree 45528f8a9f033f93676c97a8c65cfb339f4465ca

--- index.js
+++ index.js
@@ -1,34 +1,67 @@
-// Write your solution here!
+// // Write your solution here!
+// const cats = [ "Milo", "Otis", "Garfield" ];
+
+// function destructivelyAppendCat (name)
+// {
+//     return cats.push(name);
+// }
+// function destructivelyPrependCat(name) {
+//   return cats.unshift(name);
+// }
+// function destructivelyRemoveLastCat() {
+//   return cats.pop();
+// }
+// function destructivelyRemoveFirstCat() {
+//   return cats.shift();
+// }
+// function appendCat ( name )
+// {
+//     return [...cats, name]
+// }
+// function prependCat ( name )
+// {
+//     return [name, ...cats]
+// }
+// function removeLastCat ()
+// {
+//     return cats.slice( 0, cats.length - 1 );
+// }
+// function removeFirstCat ()
+// {
+//     return cats.slice( 1 );
+// }
+
 const cats = [ "Milo", "Otis", "Garfield" ];
 
-function destructivelyAppendCat (name)
+function destructivelyAppendCat ( name )
 {
-    return cats.push(name);
+  return cats.push("Ralph");
 }
-function destructivelyPrependCat(name) {
-  return cats.unshift(name);
+function destructivelyPrependCat ( name )
+{
+  return cats.unshift( "Bob" );
 }
-function destructivelyRemoveLastCat() {
+function destructivelyRemoveLastCat ()
+{
   return cats.pop();
 }
-function destructivelyRemoveFirstCat() {
+function destructivelyRemoveFirstCat ()
+{
   return cats.shift();
 }
 function appendCat ( name )
 {
-    return [...cats, name]
+  return [ ...cats, ( "Broom" ) ];
 }
 function prependCat ( name )
 {
-    return [name, ...cats]
+  return [ "Arnold", ...cats ];
 }
 function removeLastCat ()
 {
-    return cats.slice( 0, cats.length - 1 );
+  return cats.slice( 0, -1 );
 }
 function removeFirstCat ()
 {
-    return cats.slice( 1 );
+  return cats.slice( 1 );
 }
-
-
